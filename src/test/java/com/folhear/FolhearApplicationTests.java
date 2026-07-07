428cd38754417e16d41975854e452dde3614824b
anacarolinaestevaodebarros8-rgb <anacarolinaestevaodebarros8@gmail.com>
2026-07-07 22:46:57 +0000

    Configura inicializacao local com H2 e teste de contexto

---
+++ src/test/java/com/folhear/FolhearApplicationTests.java
@@ -0,0 +1,12 @@
+package com.folhear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolhearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
